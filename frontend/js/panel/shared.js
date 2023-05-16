9f966dbbbf33da7339e5fa7ccd341afc4c756c7e
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-05-16 18:55:49 +0330

    Show Notifications Created

--- frontend/js/panel/shared.js
+++ frontend/js/panel/shared.js
@@ -5,6 +5,9 @@ const $ = document;
 window.addEventListener("load", () => {
 	const adminWelcomeNameElem = $.querySelector("#admin-welcome-name");
 	const adminNameElem = $.querySelector("#admin-name");
+	const notifactionsIconElem = $.querySelector("#notifications-icon");
+	const notifactionsBoxElem = $.querySelector(".home-notification-modal");
+	const notificationModalList = $.querySelector(".home-notification-modal-list");
 
 	getAdminInfos().then((admin) => {
 		// protect panel route
@@ -14,5 +17,29 @@ window.addEventListener("load", () => {
 			adminWelcomeNameElem.innerHTML = admin.name;
 			adminNameElem.innerHTML = admin.name;
 		} else location.replace("../../login.html");
+
+		notifactionsIconElem.addEventListener("mouseenter", () => {
+			notifactionsBoxElem.classList.add("active-modal-notfication");
+		});
+
+		notifactionsBoxElem.addEventListener("mouseleave", () => {
+			notifactionsBoxElem.classList.remove("active-modal-notfication");
+		});
+
+		if (admin.notifications.length) {
+			admin.notifications.forEach((notification) => {
+				notificationModalList.insertAdjacentHTML(
+					"beforeend",
+					`
+          <li class="home-notification-modal-item">
+            <span class="home-notification-modal-text">${notification.msg}</span>
+            <a href="">دیدم</a>
+          </li>
+        `
+				);
+			});
+		} else {
+
+		}
 	});
 });
